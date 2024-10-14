559949217efbfd22c4bcbbd52e7db0944906af37
RANIL K,R <ranilkr77@gmail.com>
2024-10-14 23:44:13 +0530

    Add files via upload

---
+++ MonthNames.py
@@ -0,0 +1,29 @@
+# Exercise 1
+# Name your file: MonthNames.py
+# Write a program that reads an integer value between 1 and 12 from the user and prints output the corresponding month of the year.
+# An example run of the program (numbers in bold are typed in by the user)
+# Enter the month: 3
+# Month 3 is March
+
+# month = int(input("Enter the number"))
+# months = [ "January", "February", "March", "April", "May", "June","July", "August", "September", "October",
+#            "November", "December"]
+# if 1 <= month <= 12:
+#     print(f"Month {month} is {months[month - 1]}")
+# else:
+#     print("Invalid month number!")
+
+# Exercise 2
+#
+# A certain cinema currently sells tickets for a full price of 6 pounds, but always sells tickets for half price to people who are less than 16 years old, and for a third of the price for people who are 60 years old or more.
+# An example run of the program (numbers in bold are typed in by the user)
+# Enter your age: 63
+# Your ticket costs £2.00
+#
+# age = int(input("Enter the age"))
+# if age<16:
+#     print("Price is £3")
+# elif age>=60:
+#     print("Price is £2")
+# else:
+#     print("Price is £6")
